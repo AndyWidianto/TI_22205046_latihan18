88d35e4ec5fdc839948fecb8e470789011416b8d
AndyWidianto <145949712+AndyWidianto@users.noreply.github.com>
2023-10-30 15:21:22 +0700

    summit

---
+++ Kelipatan.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kelipatan;
+
+/**
+ *
+ * @author HP
+ */
+public class Kelipatan {
+
+    public static void main(String[] args) {
+        double i;
+        for (i=0.5;i<=35.5; i+=0.5){
+            if(i%3.5==0)
+                System.out.println(i);
+            
+        
+    }
+        
+        
+    }
+}
